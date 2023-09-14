ca4d4643bb99b1add91af5a125de567e630a4ab2
Unforgettable01 <corefano-manchelotti>
2023-09-14 23:36:06 +0300

    Добил все ошибки

--- django_sprint3/blogicum/blog/models.py
+++ django_sprint3/blogicum/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from abstracts import BaseModelSpecific, BaseModel
+from blog.abstracts import BaseModelSpecific, BaseModel
 
 
 User = get_user_model()
